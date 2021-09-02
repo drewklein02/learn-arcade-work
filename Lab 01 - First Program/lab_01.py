4554fee5a1a3f950621745f4bbc2dae0b5875fde
Drew Klein <drew.klein@my.simpson.edu>
2021-09-02 15:15:24 -0500

    Lab 1

---
+++ Lab 01 - First Program/lab_01.py
@@ -0,0 +1,4 @@
+print("I have scheduled office hours with you next tuesday.")
+print("I expect these office hours to go very well, hopefully we will get my mac set up with everything I need.")
+print("I hope I do not scream \"I hate programing\".")
+print("Im sure everything will go smoothly.")
