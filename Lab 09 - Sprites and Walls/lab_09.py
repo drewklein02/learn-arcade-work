5d6e18ad4b20d566b2884915981dc639d3bd5293
Drew Klein <drew.klein@my.simpson.edu>
2021-11-10 18:49:28 -0600

    Lab 9

--- Lab 09 - Sprites and Walls/lab_09.py
+++ Lab 09 - Sprites and Walls/lab_09.py
@@ -10,7 +10,12 @@ python -m arcade.examples.sprite_move_scrolling
 import random
 import arcade
 
-SPRITE_SCALING = 0.5
+SPRITE_SCALING1 = 1.5
+SPRITE_SCALING = .5
+SPRITE_SCALING_COIN = .25
+COIN_COUNT = 30
+coin_sound = arcade.load_sound("arcade_resources_sounds_coin4.wav")
+arcade.play_sound(coin_sound)
 
 DEFAULT_SCREEN_WIDTH = 800
 DEFAULT_SCREEN_HEIGHT = 600
@@ -39,6 +44,9 @@ class MyGame(arcade.Window):
         # Sprite lists
         self.player_list = None
         self.wall_list = None
+        self.coin_list = arcade.SpriteList()
+        self.score = 0
+        self.coin_sound = arcade.load_sound("arcade_resources_sounds_coin4.wav")
 
         # Set up the player
         self.player_sprite = None
@@ -59,135 +67,164 @@ class MyGame(arcade.Window):
         self.wall_list = arcade.SpriteList()
 
         # Set up the player
-        self.player_sprite = arcade.Sprite(":resources:images/animated_characters/female_person/femalePerson_idle.png",
+        self.player_sprite = arcade.Sprite("robot_walk4.png",
                                            scale=0.4)
         self.player_sprite.center_x = 256
         self.player_sprite.center_y = 512
         self.player_list.append(self.player_sprite)
 
         for y in range(0, 1600, 64):
-            wall = arcade.Sprite(":resources:images/tiles/grassCenter.png", SPRITE_SCALING)
+            wall = arcade.Sprite("button_square_blue_pressed.png", SPRITE_SCALING1)
             wall.center_x = 0
             wall.center_y = y
             self.wall_list.append(wall)
 
         for y in range(0, 1664, 64):
-            wall = arcade.Sprite(":resources:images/tiles/grassCenter.png", SPRITE_SCALING)
+            wall = arcade.Sprite("button_square_blue_pressed.png", SPRITE_SCALING1)
             wall.center_x = 1600
             wall.center_y = y
             self.wall_list.append(wall)
 
         for x in range(0, 1600, 64):
-            wall = arcade.Sprite(":resources:images/tiles/grassCenter.png", SPRITE_SCALING)
+            wall = arcade.Sprite("button_square_blue_pressed.png", SPRITE_SCALING1)
             wall.center_x = x
             wall.center_y = 0
             self.wall_list.append(wall)
 
         for x in range(0, 1600, 64):
-            wall = arcade.Sprite(":resources:images/tiles/grassCenter.png", SPRITE_SCALING)
+            wall = arcade.Sprite("button_square_blue_pressed.png", SPRITE_SCALING1)
             wall.center_x = x
             wall.center_y = 1600
             self.wall_list.append(wall)
 
         for y in range(128, 960, 64):
-            wall = arcade.Sprite(":resources:images/tiles/grassCenter.png", SPRITE_SCALING)
+            wall = arcade.Sprite("boxCrate.png", SPRITE_SCALING)
             wall.center_x = 192
             wall.center_y = y
             self.wall_list.append(wall)
 
         for y in range(1024, 1600, 64):
-            wall = arcade.Sprite(":resources:images/tiles/grassCenter.png", SPRITE_SCALING)
+            wall = arcade.Sprite("boxCrate.png", SPRITE_SCALING)
             wall.center_x = 192
             wall.center_y = y
             self.wall_list.append(wall)
 
         for y in range(640, 1600, 64):
-            wall = arcade.Sprite(":resources:images/tiles/grassCenter.png", SPRITE_SCALING)
+            wall = arcade.Sprite("boxCrate.png", SPRITE_SCALING)
             wall.center_x = 384
             wall.center_y = y
             self.wall_list.append(wall)
 
         for y in range(0, 576, 64):
-            wall = arcade.Sprite(":resources:images/tiles/grassCenter.png", SPRITE_SCALING)
+            wall = arcade.Sprite("boxCrate.png", SPRITE_SCALING)
             wall.center_x = 384
             wall.center_y = y
             self.wall_list.append(wall)
 
         for y in range(0, 320, 64):
-            wall = arcade.Sprite(":resources:images/tiles/grassCenter.png", SPRITE_SCALING)
+            wall = arcade.Sprite("boxCrate.png", SPRITE_SCALING)
             wall.center_x = 576
             wall.center_y = y
             self.wall_list.append(wall)
 
         for y in range(384, 704, 64):
-            wall = arcade.Sprite(":resources:images/tiles/grassCenter.png", SPRITE_SCALING)
+            wall = arcade.Sprite("boxCrate.png", SPRITE_SCALING)
             wall.center_x = 576
             wall.center_y = y
             self.wall_list.append(wall)
 
         for y in range(768, 1152, 64):
-            wall = arcade.Sprite(":resources:images/tiles/grassCenter.png", SPRITE_SCALING)
+            wall = arcade.Sprite("boxCrate.png", SPRITE_SCALING)
             wall.center_x = 576
             wall.center_y = y
             self.wall_list.append(wall)
 
         for y in range(1216, 1600, 64):
-            wall = arcade.Sprite(":resources:images/tiles/grassCenter.png", SPRITE_SCALING)
+            wall = arcade.Sprite("boxCrate.png", SPRITE_SCALING)
             wall.center_x = 576
             wall.center_y = y
             self.wall_list.append(wall)
 
         for y in range(128, 1482, 64):
-            wall = arcade.Sprite(":resources:images/tiles/grassCenter.png", SPRITE_SCALING)
+            wall = arcade.Sprite("boxCrate.png", SPRITE_SCALING)
             wall.center_x = 768
             wall.center_y = y
             self.wall_list.append(wall)
 
         for y in range(0, 960, 64):
-            wall = arcade.Sprite(":resources:images/tiles/grassCenter.png", SPRITE_SCALING)
+            wall = arcade.Sprite("boxCrate.png", SPRITE_SCALING)
             wall.center_x = 960
             wall.center_y = y
             self.wall_list.append(wall)
 
         for y in range(1024, 1554, 64):
-            wall = arcade.Sprite(":resources:images/tiles/grassCenter.png", SPRITE_SCALING)
+            wall = arcade.Sprite("boxCrate.png", SPRITE_SCALING)
             wall.center_x = 960
             wall.center_y = y
             self.wall_list.append(wall)
 
         for y in range(0, 256, 64):
-            wall = arcade.Sprite(":resources:images/tiles/grassCenter.png", SPRITE_SCALING)
+            wall = arcade.Sprite("boxCrate.png", SPRITE_SCALING)
             wall.center_x = 1152
             wall.center_y = y
             self.wall_list.append(wall)
 
         for y in range(320, 1600, 64):
-            wall = arcade.Sprite(":resources:images/tiles/grassCenter.png", SPRITE_SCALING)
+            wall = arcade.Sprite("boxCrate.png", SPRITE_SCALING)
             wall.center_x = 1152
             wall.center_y = y
             self.wall_list.append(wall)
 
         for y in range(0, 128, 64):
-            wall = arcade.Sprite(":resources:images/tiles/grassCenter.png", SPRITE_SCALING)
+            wall = arcade.Sprite("boxCrate.png", SPRITE_SCALING)
             wall.center_x = 1344
             wall.center_y = y
             self.wall_list.append(wall)
 
         for y in range(192, 1472, 64):
-            wall = arcade.Sprite(":resources:images/tiles/grassCenter.png", SPRITE_SCALING)
+            wall = arcade.Sprite("boxCrate.png", SPRITE_SCALING)
             wall.center_x = 1344
             wall.center_y = y
             self.wall_list.append(wall)
 
         for y in range(1536, 1600, 64):
-            wall = arcade.Sprite(":resources:images/tiles/grassCenter.png", SPRITE_SCALING)
+            wall = arcade.Sprite("boxCrate.png", SPRITE_SCALING)
             wall.center_x = 1344
             wall.center_y = y
             self.wall_list.append(wall)
 
-        self.physics_engine = arcade.PhysicsEngineSimple(self.player_sprite, self.wall_list)
-        arcade.set_background_color(arcade.csscolor.BLUE)
+        for i in range(COIN_COUNT):
 
+            # Create the coin instance
+            # Coin image from kenney.nl
+            coin = arcade.Sprite("keyRed.png", SPRITE_SCALING_COIN)
+
+            # --- IMPORTANT PART ---
+
+            # Boolean variable if we successfully placed the coin
+            coin_placed_successfully = False
+
+            # Keep trying until success
+            while not coin_placed_successfully:
+                # Position the coin
+                coin.center_x = random.randrange(0, 1600)
+                coin.center_y = random.randrange(0, 1600)
+
+                # See if the coin is hitting a wall
+                wall_hit_list = arcade.check_for_collision_with_list(coin, self.wall_list)
+
+                # See if the coin is hitting another coin
+                coin_hit_list = arcade.check_for_collision_with_list(coin, self.coin_list)
+
+                if len(wall_hit_list) == 0 and len(coin_hit_list) == 0:
+                    # It is!
+                    coin_placed_successfully = True
+
+            # Add the coin to the lists
+            self.coin_list.append(coin)
+
+        self.physics_engine = arcade.PhysicsEngineSimple(self.player_sprite, self.wall_list)
+        arcade.set_background_color(arcade.csscolor.BLACK)
 
     def on_draw(self):
         """
@@ -203,6 +240,7 @@ class MyGame(arcade.Window):
         # Draw all the sprites.
         self.wall_list.draw()
         self.player_list.draw()
+        self.coin_list.draw()
 
         # Select the (unscrolled) camera for our GUI
         self.camera_gui.use()
@@ -216,6 +254,9 @@ class MyGame(arcade.Window):
         text = f"Scroll value: ({self.camera_sprites.position[0]:5.1f}, " \
                f"{self.camera_sprites.position[1]:5.1f})"
         arcade.draw_text(text, 10, 10, arcade.color.BLACK_BEAN, 20)
+
+        output = f"Score: {self.score}"
+        arcade.draw_text(output, 10, 40, arcade.color.WHITE, 14)
 
     def on_key_press(self, key, modifiers):
         """Called whenever a key is pressed. """
@@ -246,6 +287,15 @@ class MyGame(arcade.Window):
 
         # Scroll the screen to the player
         self.scroll_to_player()
+
+        self.coin_list.update()
+
+        coins_hit_list = arcade.check_for_collision_with_list(self.player_sprite, self.coin_list)
+
+        for coin in coins_hit_list:
+            coin.remove_from_sprite_lists()
+            self.score += 1
+            arcade.play_sound(coin_sound)
 
     def scroll_to_player(self):
         """
